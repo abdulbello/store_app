2625cafa9fe4291f7b6149d99d5fa198f75675e1
Abdul <abdulhakeemb40@gmail.com>
2023-02-08 12:31:21 +0100

    Added profile page

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-    return render(request, 'hello.html',{"name":"Abdul"})
+    return render(request, 'hello.html')
+
+
--- store_app/settings.py
+++ store_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +51,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 INTERNAL_IPS = [
     # ...
