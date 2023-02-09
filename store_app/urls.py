42d12c869b07d138315e74c4ad59aed7e9fbe38e
Abdul <abdulhakeemb40@gmail.com>
2023-02-09 09:21:35 +0100

    corrected typo

--- store_app/urls.py
+++ store_app/urls.py
@@ -22,5 +22,5 @@ from django.contrib.staticfiles.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
+    path('__debug__/', include(debug_toolbar.urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
